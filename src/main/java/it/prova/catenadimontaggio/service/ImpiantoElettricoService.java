f4c31904bd3dbcc70acb40ce37caa1aa2febaded
ClaudioBuzi001 <buziclaudio@gmail.com>
2022-06-08 10:40:36 +0200

    Implementazione Service di ImpiantoElettrico

---
+++ src/main/java/it/prova/catenadimontaggio/service/ImpiantoElettricoService.java
@@ -0,0 +1,6 @@
+package it.prova.catenadimontaggio.service;
+
+public interface ImpiantoElettricoService {
+	public void installaImpiantoElettrico();
+
+}
---
+++ src/main/java/it/prova/catenadimontaggio/service/ImpiantoElettricoServiceImpl.java
@@ -0,0 +1,11 @@
+package it.prova.catenadimontaggio.service;
+
+public class ImpiantoElettricoServiceImpl implements ImpiantoElettricoService {
+
+	@Override
+	public void installaImpiantoElettrico() {
+		System.out.println("Installato Impianto elettrico");
+
+	}
+
+}
